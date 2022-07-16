1107645f59d40878432e8374036e0dac715dbe94
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-15 23:25:31 -0700

    add boiler plate code/folders for OrderTracker

---
+++ OrderTracker/Models/Order.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic; 
+
+namespace OrderTracker.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int Date { get; set; }
+  
+
+    public Order(string title)
+    {
+      Title = title; 
+      Description = description; 
+      Price = price; 
+      Date = date; 
+    }
+  }
+}
---
+++ OrderTracker/Models/Vendor.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic; 
+
+namespace OrderTracker.Models
+{
+  public class Vendor
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { }; 
+
+    public Vendor(string description)
+    {
+      Name = name; 
+      Description = description; 
+      _instances.Add(this); 
+      Id = instances.Count; 
+    }
+  }
+}
