93c224b874d445d2ce9315bcd9f4dadfa07ebb13
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-16 15:24:12 -0700

    build out vendor views and vendor controllers

---
+++ OrderTracker/Controllers/OrderControllers.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic; 
+using System; 
+using Microsoft.AspNetCore.Mvc; 
+using OrderTracker.Models; 
+
+namespace OrderTracker.Controllers
+{
+  public class OrderController : Controller
+  {
+
+    
+  }
+}
---
+++ OrderTracker/Controllers/VendorControllers.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic; 
+using System; 
+using Microsoft.AspNetCore.Mvc; 
+using OrderTracker.Models; 
+
+namespace OrderTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll(); 
+      return View(allVendors); 
+    }
+
+    [HttpGet("/vendors/new")]
+    {
+      public ActionResult New()
+      {
+        return View(); 
+      }
+    }
+  }
+}
