ab755cee2d48a80b890fc7775fbb358dbcbde256
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-15 23:17:05 -0700

    add Tests boiler plate

---
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.TestTools
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+  }
+}
